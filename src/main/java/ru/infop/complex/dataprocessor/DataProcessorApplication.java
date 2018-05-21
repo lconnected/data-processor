a1ab1477f81fc9d1c5c9b73cedc20957a46d8120
lconnected <levas1992@gmail.com>
2018-05-21 10:52:31 +0300

    project base
    (cherry picked from commit a795922)

---
+++ src/main/java/ru/infop/complex/dataprocessor/DataProcessorApplication.java
@@ -0,0 +1,12 @@
+package ru.infop.complex.dataprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProcessorApplication.class, args);
+	}
+}
